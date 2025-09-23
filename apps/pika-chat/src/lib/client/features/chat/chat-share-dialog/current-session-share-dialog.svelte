59074de40fac917ab9ab681834f223b0fdb9c5c0
Bruce Grant <bxgrant@yahoo.com>
2025-09-23 07:37:31 -0600

    feat: needed share dialog language

--- apps/pika-chat/src/lib/client/features/chat/chat-share-dialog/current-session-share-dialog.svelte
+++ apps/pika-chat/src/lib/client/features/chat/chat-share-dialog/current-session-share-dialog.svelte
@@ -17,7 +17,10 @@
         if (chat.shareCurrentSessionState === 'disable-share-feature') {
             // THey should never see this since we disable the share feature if it is not enabled.
             result = 'Share feature is disabled';
-        } else if (chat.shareCurrentSessionState === 'shared-by-me' || chat.shareCurrentSessionState === 'shared-by-someone-else') {
+        } else if (
+            chat.shareCurrentSessionState === 'shared-by-me' ||
+            chat.shareCurrentSessionState === 'shared-by-someone-else'
+        ) {
             result = 'Chat Share Link';
         } else if (chat.shareCurrentSessionState === 'not-shared') {
             result = 'Share Chat';
@@ -32,7 +35,10 @@
         if (chat.shareCurrentSessionState === 'disable-share-feature') {
             // THey should never see this since we disable the share feature if it is not enabled.
             result = 'Share feature is disabled';
-        } else if (chat.shareCurrentSessionState === 'shared-by-me' || chat.shareCurrentSessionState === 'shared-by-someone-else') {
+        } else if (
+            chat.shareCurrentSessionState === 'shared-by-me' ||
+            chat.shareCurrentSessionState === 'shared-by-someone-else'
+        ) {
             result = 'Already shared, get the link below';
         } else if (chat.shareCurrentSessionState === 'not-shared') {
             result = 'All current and future messages in this chat will be shared';
@@ -47,8 +53,7 @@
     }
 
     function getShareUrl() {
-        
-        if (typeof window === "undefined" || !window.location) {
+        if (typeof window === 'undefined' || !window.location) {
             throw new Error('Window element not found');
         }
 
@@ -58,24 +63,15 @@
 
         return chat.getShareUrl(window.location.origin + window.location.pathname, chat.currentSession.shareId);
     }
-
-    function getValueToShowBeforeLinkIsCreated() {
-        
-        if (typeof window === "undefined" || !window.location) {
-            throw new Error('Window element not found');
-        }
-
-        return chat.getShareUrlMock(window.location.origin + window.location.pathname);
-    }
-
-
 </script>
 
-
 <Dialog.Root
-    bind:open={() => chat.showCurrentSessionDialog, (val) => {
-        chat.showCurrentSessionDialog = val === true;
-    }}
+    bind:open={
+        () => chat.showCurrentSessionDialog,
+        (val) => {
+            chat.showCurrentSessionDialog = val === true;
+        }
+    }
     onOpenChange={(open) => {
         console.log('open', open);
         if (!open) {
@@ -88,14 +84,26 @@
             <Dialog.Title>{title}</Dialog.Title>
             <Dialog.Description>{description}</Dialog.Description>
         </Dialog.Header>
-        <div class="pt-3 max-w-[600px]">
-            <CreateCopyLinkButton 
-            width={600}
-            truncateAfter={34}
-            linkUrl={chat.shareCurrentSessionState === 'shared-by-me' || chat.shareCurrentSessionState === 'shared-by-someone-else' ? getShareUrl() : undefined}
-            createLinkFn={() => {
-                chat.createSharedSession(chat.currentSession.sessionId)
-            }} creatingLink={chat.sharingSession} />
+        <div class="pt-3 max-w-[650px]">
+            <div class="text-sm text-muted-foreground mb-2">
+                {#if chat.entityFeatureEnabled}
+                    <p>Link only works for authenticated users in your org or internal admins.</p>
+                {:else}
+                    <p>Link works for authenticated users with access to this chat app or internal admins.</p>
+                {/if}
+            </div>
+            <CreateCopyLinkButton
+                width={650}
+                truncateAfter={34}
+                linkUrl={chat.shareCurrentSessionState === 'shared-by-me' ||
+                chat.shareCurrentSessionState === 'shared-by-someone-else'
+                    ? getShareUrl()
+                    : undefined}
+                createLinkFn={() => {
+                    chat.createSharedSession(chat.currentSession.sessionId);
+                }}
+                creatingLink={chat.sharingSession}
+            />
         </div>
         <div class="flex justify-end gap-2 flex-1 items-center">
             {#if chat.sharingSession}
